e3d63ae3947207730ebb8fcb9b189aae052a0150
Tachera Sasi <tacherasasi@gmail.com>
2025-10-07 10:51:54 +0300

    Refactor comments and update payload type in main function

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 	"net/http"
 )
 
+// The idea ihave
+// recieve res from an llm then run/execute commands 
+// so i what i think is i should wrap the block that need to be executed in a custom tag e.g <exec>...</exec>
+// then parse the response to find the tags and execute the commands inside them
+// then return the output of the command to the user
+
+
 func main() {
 	url := "http://localhost:8080/"
 
-	payload := map[string]interface{}{
+	payload := map[string]any{
 		"input": "Hello, how can I use Docker with Go?",
 	}
 	payloadBytes, _ := json.Marshal(payload)
